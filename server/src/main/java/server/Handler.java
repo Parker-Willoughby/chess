b222abfbfbdbb0072794d5ec472bfde7c6b9a9b8
Parker Willoughby <pwillou2@byu.edu>
2026-07-14 11:18:37 -0700

    impl(p3) Began making handler class

---
+++ server/src/main/java/server/Handler.java
@@ -0,0 +1,14 @@
+package server;
+
+import com.google.gson.Gson;
+
+public class Handler {
+
+    public Handler() {
+        var serializer = new Gson();
+    }
+
+    public var Register(){
+        serializer.fromJson()
+    }
+}
