747d9afaadcb06ed335cacb0509d5be3e0a2a8aa
Parker Willoughby <pwillou2@byu.edu>
2026-07-20 23:22:21 -0600

    impl(p4) More changes to SQL UserDAO

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -19,6 +19,30 @@ public class SQLUserDAO {
         int id = executeUpdate(statement, data.username(), data.password(), data.email());
     }
 
+    public static UserData getUser(int id) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT id, json FROM pet WHERE id=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, id);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readUser(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Error");
+        }
+        return null;
+    }
+
+    private static UserData readUser(ResultSet rs) throws SQLException {
+        var id = rs.getInt("id");
+        var json = rs.getString("json");
+        UserData user = new Gson().fromJson(json, UserData.class);
+        return user.setId(id);
+    }
+
     private static int executeUpdate(String statement, Object... params) throws DataAccessException{
         try (Connection conn = DatabaseManager.getConnection()) {
             try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -15,8 +15,8 @@ public class UserService {
         UserData user = UserDAO.getUser(registerRequest.username());
         if (user == null) {
             String newPassword = encryptUserPassword(registerRequest.username(), registerRequest.password());
-            UserData user = new UserData(registerRequest.username(), newPassword, registerRequest.email());
-            UserDAO.createUser(registerRequest);
+            UserData newUser = new UserData(registerRequest.username(), newPassword, registerRequest.email());
+            UserDAO.createUser(newUser);
             AuthData authData = new AuthData(generateToken(), registerRequest.username());
             AuthDAO.createAuth(authData);
             return new RegisterResult(registerRequest.username(), authData.authToken());
@@ -28,7 +28,7 @@ public class UserService {
 
     public static RegisterResult login(LoginRequest loginRequest) throws DataAccessException {
         UserData user = UserDAO.getUser(loginRequest.username());
-        if (user != null && user.password().equals(loginRequest.password())) {
+        if (user != null && BCrypt.checkpw(loginRequest.password(), user.password())) {
             AuthData authData = new AuthData(generateToken(), loginRequest.username());
             AuthDAO.createAuth(authData);
             return new RegisterResult(loginRequest.username(), authData.authToken());
@@ -65,13 +65,6 @@ public class UserService {
         String hashedPassword = BCrypt.hashpw(clearTextPassword, BCrypt.gensalt());
         // write the hashed password in database along with the user's other information
         return hashedPassword;
-    }
-
-    private static boolean verifyUser(String username, String providedClearTextPassword) {
-        // read the previously hashed password from the database
-        var hashedPassword = readHashedPasswordFromDatabase(username);
-
-        return BCrypt.checkpw(providedClearTextPassword, hashedPassword);
     }
 
 }
