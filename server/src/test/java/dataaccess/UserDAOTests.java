18fa3afa1b1e8b68d10c18a83ad19a838c784291
Parker Willoughby <pwillou2@byu.edu>
2026-07-21 13:03:16 -0600

    impl(p4) Created UserDAO tests

--- server/src/test/java/dataaccess/UserDAOTests.java
+++ server/src/test/java/dataaccess/UserDAOTests.java
@@ -9,10 +9,46 @@ import javax.xml.crypto.Data;
 public class UserDAOTests {
     @Test
     public void createUserSuccess() throws DataAccessException {
-        UserData user = new UserData("username", "passwrod", "email");
+        SQLUserDAO.clear();
+        UserData user = new UserData("username", "password", "email");
         SQLUserDAO.createUser(user);
         Assertions.assertEquals(user, SQLUserDAO.getUser("username"));
-        UserDAO.clear();
+        SQLUserDAO.clear();
+    }
+
+    @Test
+    public void createUserFail() throws DataAccessException{
+        SQLUserDAO.clear();
+        UserData user = new UserData("username", "password", "email");
+        SQLUserDAO.createUser(user);
+        Assertions.assertThrows(DataAccessException.class, () -> SQLUserDAO.createUser(user));
+        SQLUserDAO.clear();
+    }
+
+    @Test
+    public void getUserSuccess() throws DataAccessException {
+        SQLUserDAO.clear();
+        UserData user = new UserData("joe", "1234", "email.email");
+        SQLUserDAO.createUser(user);
+        Assertions.assertEquals(user, SQLUserDAO.getUser("joe"));
+        SQLUserDAO.clear();
+    }
+
+    @Test
+    public void getUserFail() throws DataAccessException {
+        SQLUserDAO.clear();
+        Assertions.assertEquals(SQLUserDAO.getUser("jeff"), null);
+        SQLUserDAO.clear();
+    }
+
+    @Test
+    public void clearSuccess() throws DataAccessException {
+        UserData user = new UserData("username", "password", "email");
+        UserData user2 = new UserData("username2", "password", "email");
+        SQLUserDAO.createUser(user);
+        SQLUserDAO.createUser(user2);
+        SQLUserDAO.clear();
+        Assertions.assertTrue(SQLUserDAO.isEmpty());
     }
 
 }
